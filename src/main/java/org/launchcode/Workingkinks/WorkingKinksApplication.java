5a65b27b17a716994c8637ebe910f32cfab7e323
The Rebellion Queen <azaggy1@outlook.com>
2021-04-29 17:30:54 -0500

    Starting

---
+++ src/main/java/org/launchcode/Workingkinks/WorkingKinksApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Workingkinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingKinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingKinksApplication.class, args);
+	}
+
+}
